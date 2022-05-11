28dad107b5f1a15174648572a324847e25cdfff0
HayeonP <kite9240@gmail.com>
2022-05-11 19:51:19 +0900

    tinyxml xml bug is fixed

--- yocto_tools/create_recipes.py
+++ yocto_tools/create_recipes.py
@@ -52,7 +52,8 @@ def extract_ros_recipe_info(ros_package_name, ros_package_path):
             dependency = line.split('>')[1]
             dependency = dependency.split('<')[0]
             dependency = dependency.replace('_','-')
-            if dependency is 'tinyxml':
+
+            if dependency == 'tinyxml':
                 dependency = 'tinyxml-vendor'
             if dependency not in info.dependency:
                 info.dependency.append(dependency)
