f6e41864f1b9ac5e2c7a44739f5d6ac5130c17c9
HayeonP <kite9240@gmail.com>
2022-05-14 12:57:10 +0900

    lic files chksum for lidar_localizer is added

--- yocto_tools/create_recipes.py
+++ yocto_tools/create_recipes.py
@@ -46,8 +46,8 @@ def extract_ros_recipe_info(ros_package_name, ros_package_path):
             info.license = license
             
             lic_files_chksum = 'file://package.xml;beginline='+str(line_num)+';endline='+str(line_num)+';md5='+hashlib.md5(line.encode()).hexdigest()
-            # if ros_package_name == 'lidar_localizer':
-            #     lic_files_chksum = 
+            if ros_package_name == 'lidar_localizer':
+                lic_files_chksum = "7d942f6573dba1c22962da3cdf66dbc0"
             info.lic_files_chksum = lic_files_chksum
             
         elif 'depend>' in line and 'buildtool_depend' not in line:
