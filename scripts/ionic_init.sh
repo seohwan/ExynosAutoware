9f90d51c4fc4631683b53ed6681e9d92eff2f085
RUBIS <rubis.chief@gmail.com>
2022-05-23 14:15:31 +0900

    Update ionic_init.sh

--- scripts/ionic_init.sh
+++ scripts/ionic_init.sh
@@ -2,4 +2,4 @@ source /opt/ros/melodic/setup.bash
 export ROS_MASTER_URI="http://192.168.0.4:11311"
 export ROS_IP=192.168.0.4
 export USER_HOME="/home/root"
-export PS1="\u@\h:\w#"
+export PS1="\u@\h:\w# "
