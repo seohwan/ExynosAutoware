8c18e99dc95009112eb7e9bd608809c4ba39887d
HayeonP <kite9240@gmail.com>
2022-05-25 16:50:01 +0900

    scripts are modified

--- scripts/ionic_init.sh
+++ scripts/ionic_init.sh
@@ -1,5 +1,7 @@
 source /opt/ros/melodic/setup.bash
 source /home/root/catkin_ws/devel/setup.bash
+source /home/root/autoware.ai/devel/setup.bash
+source /home/root/rubis_ws/devel/setup.bash
 export ROS_MASTER_URI="http://192.168.0.4:11311"
 export ROS_IP=192.168.0.4
 export USER_HOME="/home/root"
