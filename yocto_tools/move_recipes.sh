89f6763de5ddfbc92ee1237b7aaa27b3e061fa51
HayeonP <kite9240@gmail.com>
2022-06-03 11:28:08 +0900

    move_recipes is modified

--- yocto_tools/move_recipes.sh
+++ yocto_tools/move_recipes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd recipes
 zip -r recipes.zip *
-scp -P 2222 recipes.zip exynos@uranium.snu.ac.kr:/home/exynos/ssd/hypark/ros/sources/meta-samsung-virt/meta-sys/recipes-autoware
+scp -P 2222 recipes.zip exynos@uranium.snu.ac.kr:/home/exynos/ssd/hypark/yocto_ws/sources/meta-samsung-virt/meta-sys/recipes-autoware
