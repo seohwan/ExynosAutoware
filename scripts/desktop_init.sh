917da6689285257cf896dfbacb94ddee93e2030f
HayeonP <kite9240@gmail.com>
2023-01-11 11:44:56 +0900

    Update IP address for desktop

--- scripts/desktop_init.sh
+++ scripts/desktop_init.sh
@@ -2,7 +2,7 @@ source /opt/ros/melodic/setup.bash
 source /home/root/catkin_ws/devel/setup.bash
 source /home/root/autoware.ai/devel/setup.bash
 source /home/root/rubis_ws/devel/setup.bash
-export ROS_MASTER_URI="http://192.168.0.66:11311"
-export ROS_IP=192.168.0.66
+export ROS_MASTER_URI="http://192.168.0.8:11311"
+export ROS_IP=192.168.0.8
 export USER_HOME="/home/root"
 export PS1="\u@\h:\w# "
