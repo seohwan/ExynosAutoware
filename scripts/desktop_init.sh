6782ec1ea35d94dd78437fc59423602a3f1da817
HayeonP <kite9240@gmail.com>
2023-01-12 13:26:43 +0900

    Remove setup.bash sourcing in desktop_init.sh

--- scripts/desktop_init.sh
+++ scripts/desktop_init.sh
@@ -1,7 +1,4 @@
 source /opt/ros/melodic/setup.bash
-source /home/root/catkin_ws/devel/setup.bash
-source /home/root/autoware.ai/devel/setup.bash
-source /home/root/rubis_ws/devel/setup.bash
 export ROS_MASTER_URI="http://192.168.0.8:11311"
 export ROS_IP=192.168.0.8
 export USER_HOME="/home/root"
