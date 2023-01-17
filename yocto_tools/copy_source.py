b88df92e3dd8db9b9304d6137dcc89bb338089cb
HayeonP <kite9240@gmail.com>
2023-01-17 19:09:20 +0900

    Fix copy_source script

--- yocto_tools/copy_source.py
+++ yocto_tools/copy_source.py
@@ -6,8 +6,13 @@ paths = str(os.popen('find ../autoware.ai/src').read())
 exynos_autoware_paths = paths.split('\n')
 
 remove_idx = []
-for i, line in enumerate(paths):
-    if 'CMakeList' in line or 'package.xml' in line or '.launch' in line: remove_idx.append(i)
+for i, line in enumerate(exynos_autoware_paths):
+    if 'CMakeList' in line:
+        remove_idx.append(i)
+    elif 'package.xml' in line:
+        remove_idx.append(i)
+    elif '.launch' in line: 
+        remove_idx.append(i)
 
 for index in sorted(remove_idx, reverse=True):
     del paths[index]
@@ -15,6 +20,7 @@ for index in sorted(remove_idx, reverse=True):
 autoware_copy_src_paths = []
 for line in paths:
     autoware_copy_src_paths.append(line.replace('../','~/'))
+
 
 for i in range(len(remove_idx)):
     os.system('mv '+ autoware_copy_src_paths[i] + ' ' + exynos_autoware_paths[i])
@@ -25,7 +31,7 @@ paths = str(os.popen('find ../rubis_ws/src').read())
 exynos_rubis_paths = paths.split('\n')
 
 remove_idx = []
-for i, line in enumerate(paths):
+for i, line in enumerate(exynos_rubis_paths):
     if 'CMakeList' in line or 'package.xml' in line or '.launch' in line: remove_idx.append(i)
 
 for index in sorted(remove_idx, reverse=True):
@@ -37,3 +43,5 @@ for line in paths:
 
 for i in range(len(remove_idx)):
     os.system('mv '+ rubis_copy_src_paths[i] + ' ' + exynos_rubis_paths[i])
+
+print('finish')
