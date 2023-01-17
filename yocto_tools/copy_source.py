ffa677e882412cada88bbcdd7f8ae4c874a7adbe
HayeonP <kite9240@gmail.com>
2023-01-17 19:06:00 +0900

    Add copy_source script

---
+++ yocto_tools/copy_source.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+# Autoware
+paths = str(os.popen('find ../autoware.ai/src').read())
+exynos_autoware_paths = paths.split('\n')
+
+remove_idx = []
+for i, line in enumerate(paths):
+    if 'CMakeList' in line or 'package.xml' in line or '.launch' in line: remove_idx.append(i)
+
+for index in sorted(remove_idx, reverse=True):
+    del paths[index]
+
+autoware_copy_src_paths = []
+for line in paths:
+    autoware_copy_src_paths.append(line.replace('../','~/'))
+
+for i in range(len(remove_idx)):
+    os.system('mv '+ autoware_copy_src_paths[i] + ' ' + exynos_autoware_paths[i])
+
+
+# rubis_ws
+paths = str(os.popen('find ../rubis_ws/src').read())
+exynos_rubis_paths = paths.split('\n')
+
+remove_idx = []
+for i, line in enumerate(paths):
+    if 'CMakeList' in line or 'package.xml' in line or '.launch' in line: remove_idx.append(i)
+
+for index in sorted(remove_idx, reverse=True):
+    del paths[index]
+
+rubis_copy_src_paths = []
+for line in paths:
+    rubis_copy_src_paths.append(line.replace('../','~/'))
+
+for i in range(len(remove_idx)):
+    os.system('mv '+ rubis_copy_src_paths[i] + ' ' + exynos_rubis_paths[i])
