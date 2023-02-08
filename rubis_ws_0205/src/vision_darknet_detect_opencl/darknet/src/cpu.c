2ec223c0de3565c96778a64dfc774059dbcd2c06
HayeonP <kite9240@gmail.com>
2023-02-08 10:58:14 +0900

    branch op_ros_helpers is created.

--- rubis_ws_0205/src/vision_darknet_detect_opencl/darknet/src/cpu.c
+++
@@ -1,8 +0,0 @@
-#include "opencl.h"
-
-#ifndef GPU_INDEX
-#define GPU_INDEX
-#ifndef GPU
-int gpu_index = -1;
-#endif
-#endif
--- yocto_tools/move_recipes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-WORKSPACE_PATH="/home/hayeonp/Exynos/ws"
-cd recipes
-zip -r recipes.zip *
-sudo rm -r $WORKSPACE_PATH/sources/meta-samsung-virt/meta-sys/recipes-autoware/*
-cp recipes.zip $WORKSPACE_PATH/sources/meta-samsung-virt/meta-sys/recipes-autoware
-cd $WORKSPACE_PATH/sources/meta-samsung-virt/meta-sys/recipes-autoware
-unzip -o recipes.zip
-rm -r recipes.zip
