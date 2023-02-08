69a9f9e72236b33a4fb64c6c27de1185ad1d5642
HayeonP <kite9240@gmail.com>
2023-02-08 11:00:10 +0900

    branch vesc_driver is created.

--- src/vesc_driver_node.cpp
+++ src/vesc_driver_node.cpp
@@ -1,14 +1,14 @@
 #include <ros/ros.h>
 
-#include "vesc_ackermann/vesc_to_odom.h"
+#include "vesc_driver/vesc_driver.h"
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "vesc_to_odom_node");
+  ros::init(argc, argv, "vesc_driver_node");
   ros::NodeHandle nh;
   ros::NodeHandle private_nh("~");
 
-  vesc_ackermann::VescToOdom vesc_to_odom(nh, private_nh);
+  vesc_driver::VescDriver vesc_driver(nh, private_nh);
 
   ros::spin();
 
